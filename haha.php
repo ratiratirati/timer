477dffc37cd187824c2b69b12afd68ad242748b8
ratiratirati <jiqia.rati@bk.ru>
2019-02-14 05:04:00 -0800

    1
    1

---
+++ haha.php
@@ -0,0 +1,25 @@
+<html>
+<head>
+    <meta charset="utf-8">
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.1/jquery.min.js"></script>
+    <link rel="stylesheet" type="text/css" href="css/style.css">
+    <link rel="stylesheet" type="text/css" href="css/fonts.css">
+</head>
+<body>
+<script>
+    function countDown(secs,elem) {
+        var element = document.getElementById(elem);
+        element.innerHTML = "<p>გთხოვთ მოითმინოთ: ( "+secs+" ) წამი</p>";
+        if(secs < 1) {
+            clearTimeout(timer);
+            element.innerHTML = '<p>დროის ათვლა მორჩა!</p>';
+            element.innerHTML += '<a href="hahaha.php">დააჭირეთ და გადადით</a>';
+        }
+        secs--;
+        var timer = setTimeout('countDown('+secs+',"'+elem+'")',1000);
+    }
+</script>
+<div id="status"></div>
+<script>countDown(120,"status");</script>
+</body>
+</html>
---
+++ hahaha.php
@@ -0,0 +1,12 @@
+<html>
+<head>
+    <meta charset="utf-8">
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.1/jquery.min.js"></script>
+    <link rel="stylesheet" type="text/css" href="css/style.css">
+    <link rel="stylesheet" type="text/css" href="css/fonts.css">
+</head>
+<body>
+
+<h1 class="fff">ჰა ჰა ჰა თაკო ხო მოგეშლალა ნერვები ;დ :d<br><img src="img/smile.png" style="width: 300px; margin-top: 20px;"></h1>
+</body>
+</html>
